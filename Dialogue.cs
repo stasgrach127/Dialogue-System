defd959b99a6233d21aa13bad1db8a1daf9c4777
stasgrach <61805309+stasgrach127@users.noreply.github.com>
2020-11-01 23:38:34 +0300

    движение камеры, улучшенная работа со спрайтами

--- Dialogue.cs
+++ Dialogue.cs
@@ -20,6 +20,8 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private string[] nameDialogue;
     [SerializeField] private Sprite[] spriteDialogue;
     [SerializeField] private AudioClip[] phrasesDialogue;
+    [SerializeField] private Transform[] positionDialogue;
+    [SerializeField] private SpriteRenderer[] personTalkingDialogue;
     int count;
     [TextArea(4,8)]
     [SerializeField] private string[] textDialogue;
@@ -27,10 +29,13 @@ public class Dialogue : MonoBehaviour
 
     #region остальные референсы
     [Header("Остальные Референсы")]
-    [SerializeField] private SpriteRenderer characterDialogue;
+    //[SerializeField] private SpriteRenderer characterDialogue;
     [SerializeField] private GameObject playerDialogue;
+    [SerializeField] private GameObject cameraDialogue;
     bool triggerCheck;
     bool canGo;
+    bool AutoMode;
+    Vector3 startPosition;
     #endregion
 
     void Start()
@@ -42,13 +47,16 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && triggerCheck && canGo)
-        {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && triggerCheck && canGo)
+        { 
             StopCoroutine("PlayText");
             NewPhrase();
             canGo = false;
             StartCoroutine("Proceed");
-            //Debug.Log(textHolder.text);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AutoMode = true;
         }
     }
 
@@ -56,13 +64,11 @@ public class Dialogue : MonoBehaviour
     private void HideDialogue()
     {
         dialogueCanvas.SetActive(false);
-        characterDialogue.enabled = true;
     }
 
     private void ShowDialogue()
     {
         dialogueCanvas.SetActive(true);
-        characterDialogue.enabled = !characterDialogue.enabled;
     }
 
     void OnTriggerEnter(Collider check)
@@ -86,26 +92,40 @@ public class Dialogue : MonoBehaviour
         if (!dialogueCanvas.activeSelf)
         {
             ShowDialogue();
+            startPosition = cameraDialogue.transform.position;
+            playerDialogue.GetComponent<Renderer>().enabled = false;
         }
         if (count != textDialogue.Length)
         {
             textHolder.text = "";
             StartCoroutine("PlayText");
-            //textHolder.text = textDialogue[count];
+            StartCoroutine("SmoothCamera");
             nameHolder.text = nameDialogue[count];
             imageHolder.sprite = spriteDialogue[count];
-            characterDialogue.sprite = spriteDialogue[count];
+            personTalkingDialogue[count].sprite = spriteDialogue[count];
             audioDialogue.clip = phrasesDialogue[count];
             audioDialogue.Play();
-            count++;
+            if (count > 0)
+            {
+                personTalkingDialogue[count].enabled = false;
+                personTalkingDialogue[count - 1].enabled = true;
+            }
+            else
+            {
+                personTalkingDialogue[count].enabled = false;
+            }
+            count += 1;
         }
         else
         {
             HideDialogue();
+            StartCoroutine("SmoothCameraEnd");
+            personTalkingDialogue[count - 1].enabled = true;
             textHolder.text = "tochange";
             nameHolder.text = "tochange";
             imageHolder.sprite = null;
             audioDialogue.Stop();
+            playerDialogue.GetComponent<Renderer>().enabled = true;
             count = 0;
         }
     }
@@ -126,6 +146,37 @@ public class Dialogue : MonoBehaviour
         yield return new WaitForSeconds(0.6f);
         canGo = true;
     }
+
+    IEnumerator AutoDialogue()
+    {
+        yield return new WaitForSeconds(2);
+        NewPhrase();
+    }
+
+    IEnumerator SmoothCamera()
+    {
+        float timeToArrive = 0f;
+        Vector3 startPos = cameraDialogue.transform.position;
+        Vector3 endPos = positionDialogue[count].position;
+        while (timeToArrive < 1)
+        {
+            timeToArrive += Time.deltaTime / 0.2f;
+            cameraDialogue.transform.position = Vector3.Lerp(startPos, endPos, timeToArrive);
+            yield return null;
+        }
+    }
+
+    IEnumerator SmoothCameraEnd() //костыль, позже исправлю
+    {
+        float timeToArrive = 0f;
+        Vector3 startPos = cameraDialogue.transform.position;
+        Vector3 endPos = startPosition;
+        while (timeToArrive < 1)
+        {
+            timeToArrive += Time.deltaTime / 0.2f;
+            cameraDialogue.transform.position = Vector3.Lerp(startPos, endPos, timeToArrive);
+            yield return null;
+        }
+    }
     #endregion
 }
-
